4b932bf4a2d27d300deba72ed43eb120cf380a0d
Ramdas Gupta <er.rdgupta@gmail.com>
2023-02-02 19:03:50 +0530

    react

---
+++ src/About.js
@@ -0,0 +1,6 @@
+
+function About() {
+    return <h2>Heading</h2>
+}
+
+export default About;
---
+++ src/components/HomeComponent.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class HomeComponent extends Component {
+     constructor(){
+        super();
+        console.log("Hello World")
+     }
+
+    render() {
+        return <h1>HomeComponent</h1>
+    }
+
+}
+
+export default HomeComponent
