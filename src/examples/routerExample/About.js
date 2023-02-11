8e8ea8b956343525f73a33bb7c972276fd7716ae
Ramdas Gupta <er.rdgupta@gmail.com>
2023-02-11 17:48:39 +0530

    router example

---
+++ src/examples/routerExample/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>                
+                <h2>This is About page</h2>
+            </div>
+        )
+    }
+}
